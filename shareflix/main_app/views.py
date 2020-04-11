c61c80a34f72df448337a470d57950e13eea77c8
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-04-11 13:38:32 -0400

    add login page and auth functionality

--- shareflix/main_app/views.py
+++ shareflix/main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(req):
+    return render(req, 'home.html')
+
+def info(req):
+    return render(req, 'info.html')
