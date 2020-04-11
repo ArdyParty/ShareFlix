92fd4f0dd311f8a7c0fd211dffb9ade137928352
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-04-11 14:00:35 -0400

    tracking changes in urls

---
+++ shareflix/main_app/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [ 
+    path('home', views.home, name="home"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('<int:user.id>', views.user, name='profile'),
+]
+
+
+
+
+# Built in URLs for django.contrib.auth.urls
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done']
+# accounts/password_reset/ [name='password_reset']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/reset/done/ [name='password_reset_complete']
