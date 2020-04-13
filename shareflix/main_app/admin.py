e43b753ca82da997fed829d1b4448834f699751f
Brandon Bernal <bernal.brandon@gmail.com>
2020-04-13 12:35:22 -0400

    Add Profile model and register to admin portal

--- shareflix/main_app/admin.py
+++ shareflix/main_app/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Profile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
 # Register your models here.
 admin.site.register(Movie)
+# admin.site.register(Profile)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+# https://simpleisbetterthancomplex.com/tutorial/2016/11/23/how-to-add-user-profile-to-django-admin.html
