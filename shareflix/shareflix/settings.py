0bfa83c46022593ade33077126f1f63d5f915282
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-04-15 16:30:51 -0400

    merge migrations

--- shareflix/shareflix/settings.py
+++ shareflix/shareflix/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/home'
+LOGIN_REDIRECT_URL = '/profile/'
 LOGOUT_REDIRECT_URL = '' # '/'
